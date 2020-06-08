e1cdfab9b6834fee96cfde3304200ea2287bef5e
Jason Woods <jason.woods@datatri.be>
2020-06-08 06:48:14 -0500

    Moving config externally so we can change logging level on the fly?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,10 @@ export class AppComponent implements OnInit {
   // constructor(private fb: FormBuilder, private ws: LoggingService) {}
   constructor(private fb: FormBuilder, private ls: LoggingService) {}
 
+
+  // We want to put in an app initializer
+  // https://stackoverflow.com/questions/49707830/angular-how-to-correctly-implement-app-initializer
+  // https://devblogs.microsoft.com/premier-developer/angular-how-to-editable-config-files/
   ngOnInit(): void {
     this.workingLoggedForm = this.fb.group({
       workingField: [''],
@@ -34,29 +38,48 @@ export class AppComponent implements OnInit {
     this.throwErrorForm = this.fb.group({
       thisField: [''],
     });
+
+    this.ls.sendDebugLevelMessage('ngOnInit message', this, {
+      error: 'none',
+    });
   }
 
   successfulSubmit() {
     // this is how we often do it. We should use a logging library!
-    console.log('Successful submit');
-    this.ls.sendInfoLevelMessage('Message submitted', this, this.workingLoggedForm.controls.workingField);
+    this.ls.sendDebugLevelMessage('Debugging successful submit', this, {
+      error: 'none',
+    });
+    this.ls.sendInfoLevelMessage('Message submitted successfully');
   }
 
   brokenSubmit() {
     // console.log('Broken submit');
     // console.log('errors ', this.brokenLoggedForm.get('brokenField').errors);
-    this.ls.sendErrorLevelMessage(
+    this.ls.sendDebugLevelMessage('Debugging broken submit', this, {
+      error: this.brokenLoggedForm.controls.brokenField.errors,
+    });
+
+    this.ls.sendWarnLevelMessage(
       'Submit went wrong',
-      this.brokenLoggedForm,
       this.brokenLoggedForm.controls.brokenField.errors
     );
   }
 
   throwDNSError() {
-    this.ls.sendDebugLevelMessage('Need to create a web call to URL that errors', this, {error: 'something went wrong'})
+    this.ls.sendDebugLevelMessage('Debugging DNS error on submit', this, {
+      error: 'something went wrong',
+    });
+
+    this.ls.sendErrorLevelMessage(
+      'Need to create a web call to URL that errors',
+      this,
+      { error: 'something went wrong' }
+    );
   }
 
-  throwsError() {
-    throw new Error('This is not your error');
+  throwsFatalError() {
+    const fatalError = new Error('Something went very wrong');
+    this.ls.sendFatalLevelMessage('This button is broken', this, fatalError);
+    // here we would throw fatalError;
   }
 }
--- src/app/services/logging.service.ts
+++ src/app/services/logging.service.ts
@@ -12,30 +12,38 @@ export class LoggingService {
     // this.logger.fatal('A fatal level log');
   }
 
+  // very detailed tracing
   sendTraceLevelMessage(message, source, error) {
     this.logger.trace(message, source, error);
   }
 
+  // Relatively detailed tracing used by application developers.
   sendDebugLevelMessage(message, source, error) {
     this.logger.debug(message, source, error);
   }
 
-  sendInfoLevelMessage(message, source, error) {
-    this.logger.info(message, source, error);
+  // Informational messages that might make sense to end users and
+  // system administrators, and highlight the progress of the application.
+  sendInfoLevelMessage(message) {
+    this.logger.info(message);
   }
 
   sendLogLevelMessage(message, source, error) {
     this.logger.log(message, source, error);
   }
 
-  sendWarnLevelMessage(message, source, error) {
-    this.logger.warn(message, source, error);
+  // Potentially harmful situations of interest to end users or system managers that indicate potential problems.
+  sendWarnLevelMessage(message, error) {
+    this.logger.warn(message, error);
   }
 
+  // Error events of considerable importance that will
+  // prevent normal program execution, but might still allow the application to continue running.
   sendErrorLevelMessage(message, source, error) {
     this.logger.error(message, source, error);
   }
 
+  // Very severe error events that might cause the application to terminate.
   sendFatalLevelMessage(message, source, error) {
     this.logger.fatal(message, source, error);
   }
